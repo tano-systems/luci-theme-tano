ebb3c8a75e6f47f035bdfa242bf555c6cc339211
Anton Kikin <a.kikin@tano-systems.com>
2020-04-18 05:23:11 +0300

    Add password authorization option for deploy
    Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

--- buildscripts/deploy.sh
+++ buildscripts/deploy.sh
@@ -3,7 +3,13 @@
 source ./env
 source ./vars
 
-scp -r -P ${TARGET_HOST_PORT} ${THEME_SRC_DIR}/htdocs/* ${TARGET_HOST}:/www
-scp -r -P ${TARGET_HOST_PORT} ${THEME_SRC_DIR}/luasrc/* ${TARGET_HOST}:/usr/lib/lua/5.1/luci
-scp -r -P ${TARGET_HOST_PORT} ${THEME_SRC_DIR}/root/* ${TARGET_HOST}:/
-scp -r -P ${TARGET_HOST_PORT} ${DIST_DIR}/*.css ${TARGET_HOST}:/www/luci-static/tano/
+RUN_SSHPASS=""
+if [ -n "${TARGET_PASSWORD}" ]; then
+	RUN_SSHPASS="sshpass -e"
+	export SSHPASS="${TARGET_PASSWORD}"
+fi
+
+${RUN_SSHPASS} scp -r -P ${TARGET_HOST_PORT} ${THEME_SRC_DIR}/htdocs/* ${TARGET_HOST}:/www
+${RUN_SSHPASS} scp -r -P ${TARGET_HOST_PORT} ${THEME_SRC_DIR}/luasrc/* ${TARGET_HOST}:/usr/lib/lua/5.1/luci
+${RUN_SSHPASS} scp -r -P ${TARGET_HOST_PORT} ${THEME_SRC_DIR}/root/* ${TARGET_HOST}:/
+${RUN_SSHPASS} scp -r -P ${TARGET_HOST_PORT} ${DIST_DIR}/*.css ${TARGET_HOST}:/www/luci-static/tano/
